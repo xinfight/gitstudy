c038a0673947dd8efe55561dae33b9277f06f310
xin <xin@126.com>
2019-06-03 14:30:16 +0800

    合并的代码的提交

--- src/com/sgcc/dlsc/test/MergeProject.java
+++ src/com/sgcc/dlsc/test/MergeProject.java
@@ -2,6 +2,7 @@ package com.sgcc.dlsc.test;
 
 public class MergeProject {
     public static void main(String[] args) {
+        System.out.println("主支线代码的冲突");
         System.out.println("主支线代码的冲突bbbbb");
     }
 }
